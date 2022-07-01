0b5eb4a718ba24b73d995303cef05cd036dc9b4e
todah-zg <104134795+todah-zg@users.noreply.github.com>
2022-07-01 09:40:49 +0200

    Create PostProcessing.cs

---
+++ Assets/Editor/PostProcessing.cs
@@ -0,0 +1,28 @@
+using UnityEditor;
+using UnityEditor.Callbacks;
+using UnityEditor.iOS.Xcode;
+using System.IO;
+
+public class IosBuildPostprocessor
+{
+
+    [PostProcessBuild(1)]
+    public static void EditPlist(BuildTarget target, string path)
+    {
+        if (target != BuildTarget.iOS)
+            return;
+
+
+        string plistPath = path + "/Info.plist";
+        PlistDocument plist = new PlistDocument();
+        plist.ReadFromFile(plistPath);
+
+        PlistElementDict rootDict = plist.root;
+
+        // Add ITSAppUsesNonExemptEncryption to Info.plist
+        rootDict.SetString("ITSAppUsesNonExemptEncryption", "false");
+
+
+        File.WriteAllText(plistPath, plist.WriteToString());
+    }
+}
